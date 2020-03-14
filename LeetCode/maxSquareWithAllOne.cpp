6bc03a3e460eaca6cdc5b449d759b247ccff0769
Prem Kumar <premku@vmware.com>
2020-03-14 20:07:43 +0530

    added max square and longestStringwithoutRepeat problem

---
+++ LeetCode/maxSquareWithAllOne.cpp
@@ -0,0 +1,47 @@
+/*
+Given a 2D binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+
+Example:
+
+Input: 
+
+1 0 1 0 0
+1 0 1 1 1
+1 1 1 1 1
+1 0 0 1 0
+
+Output: 4
+*/
+
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int rows = matrix.size();
+        if(rows == 0) return 0;
+        int cols = matrix[0].size();
+        if(cols == 0) return 0;
+        int maxSquare[rows][cols];
+        int maxSize = 0;
+        memset(maxSquare, 0, sizeof(maxSquare));
+        for(int i = 0; i< rows; i++)
+        {
+            for(int j = 0; j < cols; j++)
+            {
+                if(i ==0 ||j ==0)
+                    maxSquare[i][j] = matrix[i][j] == '1'? 1 : 0;
+                else if(matrix[i][j] =='1')
+                {
+                    maxSquare[i][j] = min(min(maxSquare[i-1][j], maxSquare[i][j-1]), maxSquare[i-1][j-1]) + 1;
+                }
+                
+                if(maxSize < maxSquare[i][j])
+                    maxSize = maxSquare[i][j];
+            }
+        }
+        return maxSize * maxSize;
+                }
+
+
+
+
+};
