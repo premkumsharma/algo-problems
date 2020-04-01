e2f2f8348109c6fe54f58bab44ed3ada4d9ad34e
Prem Kumar <premku@vmware.com>
2020-04-02 00:03:47 +0530

    added logger rate limiter

---
+++ LeetCode/google/loggerratelimiter.cpp
@@ -0,0 +1,56 @@
+
+/*
+Design a logger system that receive stream of messages along with its timestamps, each message should be printed if and only if it is not printed in the last 10 seconds.
+
+Given a message and a timestamp (in seconds granularity), return true if the message should be printed in the given timestamp, otherwise returns false.
+
+It is possible that several messages arrive roughly at the same time.
+
+Example:
+
+Logger logger = new Logger();
+
+// logging string "foo" at timestamp 1
+logger.shouldPrintMessage(1, "foo"); returns true; 
+
+// logging string "bar" at timestamp 2
+logger.shouldPrintMessage(2,"bar"); returns true;
+
+// logging string "foo" at timestamp 3
+logger.shouldPrintMessage(3,"foo"); returns false;
+
+// logging string "bar" at timestamp 8
+logger.shouldPrintMessage(8,"bar"); returns false;
+
+*/
+class Logger {
+    map<string,int> messageTimeStamp;
+public:
+    /** Initialize your data structure here. */
+    Logger() {
+        
+    }
+    
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    bool shouldPrintMessage(int timestamp, string message) {
+        auto lastMessage = messageTimeStamp.find(message);
+        
+        if(lastMessage == messageTimeStamp.end())
+        {
+            messageTimeStamp.insert(pair<string,int>(message,timestamp));
+            return true;
+        }
+        if(lastMessage->second > timestamp -10) return false;
+        
+        lastMessage->second = timestamp;
+        return true;
+    }
+};
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger* obj = new Logger();
+ * bool param_1 = obj->shouldPrintMessage(timestamp,message);
+ */
